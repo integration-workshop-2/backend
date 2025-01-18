fe019a6ec5f3dac9f829680b6a57b9e882521d0c
jhcsoares <jhcsoares1@gmai.com>
2025-01-18 09:26:36 -0300

    added create_non_recognized_patients use case and parameter

---
+++ backend/data/parameters/non_recognized_patients/create_non_recognized_patient/parameter.py
@@ -0,0 +1,5 @@
+from typing import NamedTuple
+
+
+class CreateNonRecognizedPatientParameter(NamedTuple):
+    patient_id: str
---
+++ backend/data/use_cases/non_recognized_patients/create_non_recognized_patient/use_case.py
@@ -0,0 +1,21 @@
+from data.parameters.non_recognized_patients.create_non_recognized_patient import (
+    CreateNonRecognizedPatientParameter,
+)
+from infra.repo.non_recognized_patients_repository.repository import (
+    NonRecognizedPatientsRepository,
+)
+from typing import Dict
+
+
+class CreateNonRecognizedPatientUseCase:
+    def __init__(self) -> None:
+        self.__non_recognized_patients_repository = NonRecognizedPatientsRepository()
+
+    def execute(self, parameter: CreateNonRecognizedPatientParameter) -> Dict:
+        non_recognized_patient = (
+            self.__non_recognized_patients_repository.create_non_recognized_patient(
+                patient_id=parameter.patient_id
+            )
+        )
+
+        return {"success": True, "data": non_recognized_patient._asdict()}
