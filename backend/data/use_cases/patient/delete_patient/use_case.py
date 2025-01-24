2f7bc55d040650068211f5ae9997689afffc3b98
jhcsoares1 <jhcsoares1@gmail.com>
2025-01-23 23:33:58 -0300

    updated job deletion in patient use case

--- backend/data/use_cases/patient/delete_patient/use_case.py
+++ backend/data/use_cases/patient/delete_patient/use_case.py
@@ -1,15 +1,21 @@
 from data.parameters.patient.delete_patient.parameter import DeletePatientParameter
 from infra.repo.patient_repository.repository import PatientRepository
+from infra.utils.crontab_util.util import CrontabUtil
 from typing import Dict
 
 
 class DeletePatientUseCase:
     def __init__(self) -> None:
+        self.__crontab_util = CrontabUtil()
         self.__patient_repository = PatientRepository()
 
     def execute(self, parameter: DeletePatientParameter) -> Dict:
         deleted_patient = self.__patient_repository.delete_patient(
             id=parameter.patient_id
+        )
+
+        self.__crontab_util.delete_routine_job_by_patient_id(
+            patient_id=parameter.patient_id
         )
 
         return {"success": True, "data": deleted_patient._asdict()}
