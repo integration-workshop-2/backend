ac690c9b681e8c6fa70973b0460f49dd1ce08ccb
jhcsoares1 <jhcsoares1@gmail.com>
2025-01-28 20:35:45 -0300

    modified browser util

--- backend/infra/utils/browser_util/util.py
+++ backend/infra/utils/browser_util/util.py
@@ -1,12 +1,24 @@
-from selenium import webdriver
+from json import loads
+from seleniumwire import webdriver
+from typing import Dict
 
 
 class BrowserUtil:
-    def __init__(self) -> None:
+    def __init__(self, host: str = "localhost", port: str = 5500) -> None:
         self.__driver = webdriver.Firefox()
+        self.__prefix = f"http://{host}:{port}/"
 
-    def open_page(self, url: str) -> None:
-        self.__driver.get(url=url)
+    def open_page(self, endpoint: str) -> None:
+        self.__driver.get(url=f"{self.__prefix}{endpoint}")
 
     def close_page(self) -> None:
         self.__driver.close()
+
+    def get_request_info(self, endpoint: str) -> Dict:
+        for request in self.__driver.requests:
+            if request.response:
+                if endpoint in request.url:
+                    response_body = request.response.body.decode("utf-8")
+                    return loads(response_body)
+
+        return {}
