be1543bda274a618edd33eec8541225cd5ee1464
jhcsoares <jhcsoares1@gmail.com>
2025-02-05 00:12:47 -0300

    tested hw

--- backend/infra/hardware/control_motor/control_motor.py
+++ backend/infra/hardware/control_motor/control_motor.py
@@ -1,5 +1,5 @@
-from infra.hardware.motor.motor import Motor
-from infra.hardware.tcrt5000.tcrt5000 import TCRT5000
+from motor.motor import Motor
+from tcrt5000.tcrt5000 import TCRT5000
 from time import sleep
 from typing import List
 
@@ -7,8 +7,8 @@ from typing import List
 class ControlMotor:
     def __init__(self, cylinder_number: int) -> None:
         self.__motor = Motor(cylinder_number=cylinder_number)
-        self.__tcrt5000a = TCRT5000()
-        self.__tcrt5000b = TCRT5000()
+        self.__tcrt5000a = TCRT5000(sensor_pin=5)
+        self.__tcrt5000b = TCRT5000(sensor_pin=17)
 
     def execute_controlled_movement(self) -> None:
         for _ in range(512):
